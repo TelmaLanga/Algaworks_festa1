328a96e53e74c6cb305abd21d6c2935d43789a6b
Telma Langa <telmajackson@gmail.com>
2017-04-16 17:42:39 +0200

    Init Project

---
+++ src/main/java/com/projectoTelma/festa1/Festa1Application.java
@@ -0,0 +1,12 @@
+package com.projectoTelma.festa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Festa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Festa1Application.class, args);
+	}
+}
---
+++ src/test/java/com/projectoTelma/festa1/Festa1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projectoTelma.festa1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Festa1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
